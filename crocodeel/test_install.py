6c01e7894282c592a01a0af68564def4735e6515
Florian Plaza Onate <fplazaonate@fantoomy-dev.mgps.inra.fr>
2024-05-06 15:33:21 +0200

    Fix logging messages order

--- crocodeel/test_install.py
+++ crocodeel/test_install.py
@@ -7,6 +7,7 @@ import sys
 from typing import Final
 from crocodeel.easy_wf import run_easy_wf
 
+
 class TestData:
     SPECIES_ABUNDANCE_TABLE: Final[Path] = Path(
         importlib.resources.files().joinpath("test_data", "mgs_profiles_test.tsv")
@@ -19,6 +20,7 @@ class TestData:
     EXPECTED_PDF_REPORT_FILE: Final[Path] = Path(
         importlib.resources.files().joinpath("test_data", "results", "contamination_events.pdf")
     )
+
 
 def run_test_install(keep_results: bool) -> None:
     with (
@@ -39,15 +41,14 @@ def run_test_install(keep_results: bool) -> None:
         ) as pdf_report_fh,
     ):
         easy_wf_args = {
-            "species_ab_table_fh" : species_ab_table_fh,
-            "conta_events_fh" : conta_events_fh,
-            "pdf_report_fh" : pdf_report_fh,
-            "nproc" : 1
+            "species_ab_table_fh": species_ab_table_fh,
+            "conta_events_fh": conta_events_fh,
+            "pdf_report_fh": pdf_report_fh,
+            "nproc": 1,
         }
 
         logging.info("Tests on the toy dataset started")
         run_easy_wf(easy_wf_args)
-        logging.info("Tests completed successfully")
 
         if filecmp.cmp(TestData.EXPECTED_CONTA_EVENTS_FILE, conta_events_fh.name):
             logging.info("All contamination events with expected rates and probabilities found")
@@ -55,5 +56,6 @@ def run_test_install(keep_results: bool) -> None:
             logging.error("Contamination events found are not those expected")
             sys.exit(1)
 
+    logging.info("Tests completed successfully")
     if not keep_results:
         logging.info("Temporary result files deleted")
