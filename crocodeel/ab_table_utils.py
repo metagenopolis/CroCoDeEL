60b56a8744e1a10a4f903c0442c71d83550d53ec
Florian Plaza Oñate <florian.plaza-onate@inrae.fr>
2026-08-13 16:18:10 +0200

    Refactor module ab_table_utils

--- crocodeel/ab_table_utils.py
+++ crocodeel/ab_table_utils.py
@@ -1,3 +1,5 @@
+"""Utilities for reading and preprocessing species abundance tables."""
+
 import logging
 import sys
 from typing import TextIO, Optional
@@ -5,7 +7,67 @@ import numpy as np
 import pandas as pd
 
 
+def _validate_and_normalize_species_names(species_ab_table: pd.DataFrame) -> None:
+    """Check and normalize species names to strings."""
+    species_names_type = species_ab_table.index.inferred_type
+
+    if species_names_type not in ("integer", "string"):
+        logging.error(
+            "Species names in first column are of the '%s' type "
+            "but should be 'string' or 'integer'",
+            species_names_type,
+        )
+        sys.exit(1)
+
+    species_ab_table.index = species_ab_table.index.astype(str)
+
+
+def _check_numeric_abundances(species_ab_table: pd.DataFrame) -> None:
+    """Check that species abundances are numeric."""
+    bad_format_samples = [
+        sample
+        for sample in species_ab_table.columns
+        if not pd.api.types.is_numeric_dtype(species_ab_table[sample].dtype)
+    ]
+
+    if bad_format_samples:
+        logging.error(
+            "Species abundance in the following samples is not numeric: %s",
+            " ".join(bad_format_samples),
+        )
+        sys.exit(1)
+
+
+def _check_non_negative_abundances(
+    species_ab_table: pd.DataFrame,
+) -> None:
+    """Check that species abundances are non-negative."""
+    negative_samples = species_ab_table.columns[(species_ab_table < 0).any(axis=0)]
+
+    if not negative_samples.empty:
+        logging.error(
+            "The following samples contain negative species abundances: %s",
+            " ".join(negative_samples),
+        )
+        sys.exit(1)
+
+
+def _check_non_empty_samples(
+    species_ab_table: pd.DataFrame,
+) -> None:
+    """Check that each sample contains at least one non-zero abundance."""
+    empty_samples = species_ab_table.columns[species_ab_table.sum(axis=0) <= 0]
+
+    if not empty_samples.empty:
+        logging.error(
+            "The following samples have no non-zero species abundances: %s",
+            " ".join(empty_samples),
+        )
+        sys.exit(1)
+
+
 def read(fh: TextIO) -> pd.DataFrame:
+    """Read a species abundance table from a TSV file."""
     # Read table
     logging.info("Reading %s", fh.name)
     species_ab_table = pd.read_csv(fh, sep="\t", header=0, index_col=0, comment="#")
@@ -18,50 +80,22 @@ def read(fh: TextIO) -> pd.DataFrame:
         "s" if num_samples > 1 else "",
     )
 
-    # Check species names
-    species_names_type = species_ab_table.index.inferred_type
-    if species_names_type not in ("integer", "string"):
-        logging.error(
-            "Species names in first column are of the '%s' type "
-            "but should be 'string' or 'integer'",
-            species_names_type,
-        )
-        sys.exit(1)
-
-    # Convert species names to strings when they are integers
-    species_ab_table.index = species_ab_table.index.astype(str)
-
-    # Check species abundance type
-    bad_format_samples = [
-        sample
-        for sample in species_ab_table.columns
-        if not pd.api.types.is_numeric_dtype(species_ab_table[sample].dtype)
-    ]
-    if bad_format_samples:
-        logging.error(
-            "Species abundance in the following samples is not numeric: %s",
-            " ".join(bad_format_samples),
-        )
-        sys.exit(1)
-
     return species_ab_table
 
 
-def normalize(species_ab_table: pd.DataFrame) -> pd.DataFrame :
+def normalize(species_ab_table: pd.DataFrame) -> pd.DataFrame:
+    """Normalize species abundances to relative abundances."""
     # Normalize to relative abundance
     species_ab_table = species_ab_table.div(species_ab_table.sum(axis=0), axis=1)
 
-    # Perform log10 transformation
-    with np.errstate(divide="ignore"):
-        species_ab_table = species_ab_table.apply(np.log10)
-
-    logging.info("Species abundance table normalized and log-transformed")
+    logging.info("Species abundance table normalized")
     return species_ab_table
 
 
 def filter_low_ab(
     species_ab_table: pd.DataFrame, filtering_ab_thr_factor: float
 ) -> pd.DataFrame:
+    """Remove species with low abundances from each sample."""
     species_ab_table = species_ab_table.apply(
         lambda ab: np.where(ab <= filtering_ab_thr_factor * ab[ab > 0].min(), 0, ab)
     )
@@ -71,28 +105,19 @@ def filter_low_ab(
 
 
 def read_filter_normalize(
-    fh: TextIO, filtering_ab_thr_factor: Optional[float]
+    fh: TextIO, filtering_ab_thr_factor: Optional[float] = None
 ) -> pd.DataFrame:
+    """Read, validate, optionally filter, and normalize a species abundance table."""
     species_ab_table = read(fh)
+    _validate_and_normalize_species_names(species_ab_table)
+    _check_numeric_abundances(species_ab_table)
+    _check_non_negative_abundances(species_ab_table)
+    _check_non_empty_samples(species_ab_table)
+
     if filtering_ab_thr_factor is not None:
         species_ab_table = filter_low_ab(species_ab_table, filtering_ab_thr_factor)
+        _check_non_empty_samples(species_ab_table)
+
     species_ab_table = normalize(species_ab_table)
 
     return species_ab_table
-
-
-def compare_species_names(species_ab_table: pd.DataFrame, species_ab_table_2: pd.DataFrame) -> None:
-    species_names = set(species_ab_table.index)
-    species_names_2 = set(species_ab_table_2.index)
-
-    if species_names != species_names_2:
-        logging.warning(
-            "Abundance tables have only %d species names in common",
-            len(species_names.intersection(species_names_2)),
-        )
-        logging.warning(
-            "Make sure the abundance tables were generated with the same tool and database"
-        )
-        logging.warning(
-            "Missing abundance values will be filled with zeros for non-shared species"
-        )
